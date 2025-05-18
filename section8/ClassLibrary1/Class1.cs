eb0d7af62a75b9eb5974b8800509a69b9ed116c9
Sharmin Sayyed <sharminsayyed029@gmail.com>
2025-05-19 00:32:37 +0530

    added

---
+++ section8/ClassLibrary1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization;
+
+public class Employee
+{
+    public int eid; // instance field
+    public string enm;
+    public string ejob;
+
+    public static string companynm; // static field
+
+    // implicit constructor  - created by c# compiler when the class does not have a constructor
+    // does not intialize value -  and cannot be seen 
+    // if the developer provides a constructor than implicit constructor is not formed 
+
+    // below are explicit constructors 
+    // below are example of constructor overloading 
+    //  instance constructor - empty/parameter-less/default
+    public Employee()
+    {
+        eid = 1;
+    }
+
+    // instance parameterized constructor 
+    public Employee(int eid , string enm ,string ejob)
+    {
+        this.eid = eid;
+        this.enm = enm;
+        this.ejob = ejob;
+
+    }
+    public Employee(int eid , string enm)
+    {
+        this.eid = eid;
+        this.enm = enm;
+    }
+    // static constructor - public by default , cannot have parameter , no return value
+    static Employee()
+    {
+        // intialize static field
+        companynm = "tatacs";
+    }
+}
---
+++ section8/ConstructorEX/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Employee e1 = new Employee();
+        // when we call the class for execution in main method for the first time at that only the static constructor is called and executed only once 
+        Employee e2 = new Employee(2 , "sharmin","manager");
+        Employee e3 = new Employee(3, "laxmi");
+
+        // object intailizers usage 
+        Employee e4 = new Employee() { enm = "sharmin", ejob = "executive" };
+
+        Console.WriteLine("employee 1:");
+        Console.WriteLine(e1.eid);
+        Console.WriteLine(e1.enm);
+        Console.WriteLine(e1.ejob);
+        Console.WriteLine(Employee.companynm);
+        Console.WriteLine();
+        Console.WriteLine("employee 2:");
+        Console.WriteLine(e2.eid);
+        Console.WriteLine(e2.enm);
+        Console.WriteLine(e2.ejob);
+        Console.WriteLine(Employee.companynm);
+        Console.WriteLine();
+        Console.WriteLine("employee 3:");
+        Console.WriteLine(e3.eid);
+        Console.WriteLine(e3.enm);
+        Console.WriteLine(e3.ejob);
+        Console.WriteLine(Employee.companynm);
+
+        Console.ReadKey();
+    }
+}
