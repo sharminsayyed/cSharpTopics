da38a04337a3db159e6430e9efb82c606c021d4e
Sharmin Sayyed <sharminsayyed029@gmail.com>
2025-06-20 00:44:34 +0530

    added

---
+++ section15/BoxingEx/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // primitive varaible 
+        int x = 10; // stored in stack memory
+
+        // boxing -(value type datatype) to (reference type datatype)
+        // boxing happens internally / automatically 
+        // from stack memory to heap memory
+        // no need of the syntax 
+        //System.Object obj = x;
+        object obj = x; // value copied in the heap memory
+
+        Console.WriteLine(x);
+        Console.WriteLine(obj);
+
+        Console.ReadKey();
+    }
+}
---
+++ section15/UnboxingEx/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // reference type value 
+        //System.Object obj = 10;
+        object obj = 10; // stored in heap memory 
+
+        // unboxing - reference type datatype to value type datatype 
+        // from heap memory to stack memory 
+
+        int x = (int)obj; // value copied in stack memory  now 
+
+        Console.WriteLine(obj);
+        Console.WriteLine(x);
+
+        Console.ReadKey();
+    }
+}
