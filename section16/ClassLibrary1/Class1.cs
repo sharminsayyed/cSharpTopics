19a85d6c737d7784baa99d9eac0d7ff93a449fbe
Sharmin Sayyed <sharminsayyed029@gmail.com>
2025-06-21 23:51:36 +0530

    added

---
+++ section16/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿// generic class 
+//A generic class in C# allows you to create a class that works with any data type (like int, string, float, or even custom types), while maintaining type safety and code reusability.
+
+//The generic type parameter (T) acts as "temporary data type", which represents the actual data type, provided by the user, while creating object.
+//You can have multiple "generic type parameters" in the same class (for use for different fields.Generics are introduced in C# 2.0.
+public class User <T1 ,T2>
+{
+    // generic feild
+    public T1 registrationStatus;
+
+    // another generic feild
+    // both feilds can have different datatypes assigned to them 
+    public T2 age;
+}
---
+++ section16/GenericClassEx/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // create an object of generic class 
+        // here the class is same but datatypes used are different 
+        // when u want to store different types of value with respect to various objects 
+        // ypu decide the datatype of the feild while creating the object rather than while creating feild in the class 
+        // helps in code reuse , performance , type safety 
+        // we can create generic classes , interfaces methods and delegates 
+        User<int,int > u1 = new User<int,int>();
+        u1.registrationStatus = 21;
+        u1.age = 23;
+        Console.WriteLine(u1.registrationStatus);
+        Console.WriteLine(u1.age);
+
+        User<bool,string > u2 = new User<bool,string >();
+        u2.registrationStatus = false;
+        u2.age = "35 to 45";
+        Console.WriteLine(u2.registrationStatus);
+        Console.WriteLine(u2.age);
+
+        Console.ReadLine();
+
+    }
+}
