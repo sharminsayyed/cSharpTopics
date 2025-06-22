2d04322a0d0bfd4e19729d353ae35681eec3e172
Sharmin Sayyed <sharminsayyed029@gmail.com>
2025-06-22 16:36:03 +0530

    added

---
+++ section17/NullPropagationOperatorEx/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NullPropagationOperator
+{
+
+    class Person
+    {
+        public int age;
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Person p1=null; // class is nullable type by default 
+            Person p2 = new Person() { age = 34 };
+            // usage of null propagation operator 
+            // syntax - reference varaible ?. feildname 
+            // checks if the reference varaible is null or not 
+            // if null then returns null 
+            // if not null then return the feildname , method ,property for that reference varaible
+            // It accesses the property or method, only if the reference variable is "not null"; just returns "null", if the reference variable is "null".
+            //Advantage: We can invoke desired member (property or method) after checking if null.
+            Console.WriteLine(p1?.age);
+            Console.WriteLine(p2?.age);
+
+            // what the above statement means 
+            //Console.WriteLine((p1 == null) ? null: Convert.ToString(p1.age)) ;
+            //Console.WriteLine((p2 == null) ? null : Convert.ToString(p2.age));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ section17/NullableTypesEx/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Namespace1
+{
+    class Person
+    {
+        // structure , enumeration are value types 
+        // which are by default non nullable types (cannot assign null value)
+        public int? noOfchildren; // converted value type to nullable type
+        //public Nullable<int> noofchilren; another syntax
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Person p1= new Person() { noOfchildren = 2 };
+            Person p2 = new Person() { noOfchildren = 34 };// for if statement
+            Person p3 = new Person() { noOfchildren = 12 }; // for ternary condition
+            Person p4 = new Person() { noOfchildren = null }; // for null coalescing opertor 
+            Person p5 = new Person() { noOfchildren = null }; // to show blank output
+            Person p6 = null; // reference types are by default nullable types (can assign null value)
+            // reference types include interfaces , classes
+
+
+            Console.WriteLine(p1.noOfchildren);
+
+            // we can use if statement to chaeck if the value is null or not 
+            // if(p2.noOfcildren != null)
+            if (p2.noOfchildren.HasValue)
+            {
+                int x = p2.noOfchildren.Value; // use Value to access the actual value which is inside the nullable type 
+                Console.WriteLine(x);
+            }
+
+            // we can also use ternary operator 
+            Console.WriteLine((p3.noOfchildren.HasValue) ?p3.noOfchildren.Value :0);
+
+            // usage of null coalescing operator 
+            // help us to avoid the usage of if statement to check if it is null or not 
+            // syntax :varaible ?? valueIfNull
+            // if not null leftside value will be displayed if null right side value will be display 
+            Console.WriteLine(p4.noOfchildren ?? -1);
+
+
+            Console.WriteLine(p5.noOfchildren); // it is null will give a blank output 
+
+            Console.ReadKey();
+        }
+    }
+}
